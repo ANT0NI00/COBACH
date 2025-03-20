af44c9a8e6ee989f9ca187a419922fc2c4a6365a
ANT0NI00 <165732216+ANT0NI00@users.noreply.github.com>
2025-03-19 20:36:00 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const slides = document.querySelectorAll(".slide");
     let index = 0;
 
-    // Mostrar/ocultar menú en móviles
+
     menuToggle.addEventListener("click", function () {
         menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
     });
 
-    // Carrusel de imágenes
+
     function showSlide(i) {
         if (i >= slides.length) index = 0;
         if (i < 0) index = slides.length - 1;
@@ -33,12 +33,12 @@ document.addEventListener("DOMContentLoaded", function () {
         showSlide(index);
     }, 5000);
 
-    // Evitar que el menú se cierre al hacer clic en los botones del carrusel
+
     prev.addEventListener("click", function (event) {
-        event.stopPropagation(); // Esto evita que el clic afecte al menú
+        event.stopPropagation(); 
     });
 
     next.addEventListener("click", function (event) {
-        event.stopPropagation(); // Esto evita que el clic afecte al menú
+        event.stopPropagation(); 
     });
 });
--- tramites.js
+++ tramites.js
@@ -1,4 +1,4 @@
-// Script para el menú de hamburguesa
+
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.menu-container').classList.toggle('active');
 });
